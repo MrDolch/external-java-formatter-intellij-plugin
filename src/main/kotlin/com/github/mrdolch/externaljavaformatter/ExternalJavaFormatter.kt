50c6c52842e464fe88340928e544d696292ee659
Christoph Dolch <christoph.dolch@dolch.tech>
2022-12-12 23:04:22 +0100

    Switch default to extended formatting with line length of 120 characters and Release

--- src/main/kotlin/com/github/mrdolch/externaljavaformatter/ExternalJavaFormatter.kt
+++ src/main/kotlin/com/github/mrdolch/externaljavaformatter/ExternalJavaFormatter.kt
@@ -1,5 +1,6 @@
 package com.github.mrdolch.externaljavaformatter
 
+import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.SimpleJavaParameters
 import com.intellij.execution.process.CapturingProcessAdapter
 import com.intellij.execution.process.OSProcessHandler
@@ -11,63 +12,87 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.psi.PsiFile
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+import java.io.File
 import java.nio.charset.StandardCharsets
+import java.time.Duration
 
 class ExternalJavaFormatter : AsyncDocumentFormattingService() {
-    override fun getFeatures(): MutableSet<FormattingService.Feature> {
-        return mutableSetOf()
-    }
+  override fun getFeatures(): MutableSet<FormattingService.Feature> {
+    return mutableSetOf()
+  }
 
-    override fun canFormat(file: PsiFile): Boolean = file.fileType.name == "JAVA"
+  override fun getTimeout(): Duration = Duration.ofSeconds(5)
+  override fun canFormat(file: PsiFile): Boolean = file.fileType.name == "JAVA"
+  private fun getRelevantJdk(project: Project): Sdk? = ProjectRootManager.getInstance(project).projectSdk
 
-    private fun getRelevantJdk(project: Project): Sdk? = ProjectRootManager.getInstance(project).projectSdk
-    override fun createFormattingTask(formattingRequest: AsyncFormattingRequest): FormattingTask? {
-        val configuration = formattingRequest.context.project
-            .getService(PersistConfigurationService::class.java).state
+  @OptIn(InternalCoroutinesApi::class)
+  override fun createFormattingTask(formattingRequest: AsyncFormattingRequest): FormattingTask? {
+    val jdk = getRelevantJdk(formattingRequest.context.project) ?: return null
+    val file = formattingRequest.ioFile ?: return null
 
-        if (configuration.enabled != true) return null
+    val configuration = formattingRequest.context.project
+      .getService(PersistConfigurationService::class.java).state
 
-        val params = SimpleJavaParameters()
-        params.jdk = getRelevantJdk(formattingRequest.context.project)
-        params.workingDirectory = configuration.workingDir
-        params.mainClass = configuration.mainClass
-        configuration.classPath?.run {
-            trim().split("[:;]+".toRegex()).forEach(params.classPath::add)
+    if (configuration.enabled != true) return null
+
+    synchronized(this) {
+      return object : FormattingTask {
+        private var handler: OSProcessHandler? = null
+        private var isCanceled = false
+
+        override fun isRunUnderProgress(): Boolean = true
+
+        override fun cancel(): Boolean {
+          if (handler == null) isCanceled = true
+          else handler!!.destroyProcess()
+          return true
         }
-        configuration.arguments?.run {
-            trim().split("\\s+".toRegex()).forEach { argument ->
-                if (argument != "{}") params.programParametersList.add(argument)
-                else params.programParametersList.add(formattingRequest.ioFile!!.absolutePath)
-            }
+
+        override fun run() {
+          val commandLine = buildCommandLine(configuration, jdk, file)
+          if (isCanceled) return;
+          val handler = OSProcessHandler(commandLine)
+          handler.addProcessListener(object : CapturingProcessAdapter() {
+            override fun processTerminated(event: ProcessEvent) =
+              if (isCanceled) {
+              } else if (event.exitCode == 0) formattingRequest.onTextReady(output.stdout)
+              else formattingRequest.onError("FormattingError", output.stderr)
+          })
+          handler.startNotify()
         }
-        configuration.vmOptions?.run {
-            trim().split("\\n+".toRegex()).forEach(params.vmParametersList::add)
-        }
-        val handler = OSProcessHandler(params.toCommandLine().withCharset(StandardCharsets.UTF_8))
-        return object : FormattingTask {
-            override fun isRunUnderProgress(): Boolean = true
-
-            override fun cancel(): Boolean {
-                handler.destroyProcess()
-                return true
-            }
-
-            override fun run() {
-                handler.addProcessListener(object : CapturingProcessAdapter() {
-                    override fun processTerminated(event: ProcessEvent) =
-                        if (event.exitCode == 0) formattingRequest.onTextReady(output.stdout)
-                        else formattingRequest.onError("Ein Fehler ist aufgetreten ^^", output.stderr)
-                })
-                handler.startNotify()
-            }
-        }
+      }
     }
+  }
 
-    override fun getNotificationGroupId(): String {
-        return "external google-java-formatter";
+  private fun buildCommandLine(
+    configuration: PersistConfigurationService.Configuration, jdk: Sdk, file: File
+  ): GeneralCommandLine {
+    val params = SimpleJavaParameters()
+    params.jdk = jdk
+    params.workingDirectory = configuration.workingDir
+    params.mainClass = configuration.mainClass
+    configuration.classPath?.run {
+      trim().split("[:;]+".toRegex()).forEach(params.classPath::add)
     }
+    configuration.arguments?.run {
+      trim().split("\\s+".toRegex()).forEach { argument ->
+        if (argument != "{}") params.programParametersList.add(argument)
+        else params.programParametersList.add(file.absolutePath)
+      }
+    }
+    configuration.vmOptions?.run {
+      trim().split("\\n+".toRegex()).forEach(params.vmParametersList::add)
+    }
+    return params.toCommandLine().withCharset(StandardCharsets.UTF_8)
+  }
 
-    override fun getName(): String {
-        return "external google-java-formatter";
-    }
+  override fun getNotificationGroupId(): String {
+    return "external google-java-formatter";
+  }
+
+  override fun getName(): String {
+    return "external google-java-formatter";
+  }
 }
